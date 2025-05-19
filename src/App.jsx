7cee852a09367a8aef89df17f2cb330c9510ba60
Abhishek-17H <abhishekhegaje1702@gmail.com>
2025-05-19 09:34:45 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,52 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { Button } from "./components/ui/button"
-function App() {
+import AppLayout from "./layout/app-layout"
+import LandingPage from "./pages/landing"
+import Onboarding from "./pages/onboarding"
+import Jobs from "./pages/jobs"
+import JobListing from "./pages/job-listing"
+import SavedJobs from "./pages/saved-jobs"
+import PostJobs from "./pages/post-jobs"
+import { ThemeProvider } from "./components/theme.provided"
 
+function App() {
+  const router=createBrowserRouter(
+    [
+      {
+        element:<AppLayout/>,
+        children:[
+          {
+            path:"/",
+            element:<LandingPage/>
+          },
+          {
+            path:"/onboarding",
+            element:<Onboarding/>
+          },
+          {
+            path:"/job-listing",
+            element:<JobListing/>
+          },
+          {
+            path:"/jobs/:id",
+            element:<Jobs/>
+          },
+          {
+            path:"/saved-jobs",
+            element:<SavedJobs/>
+          },
+          {
+            path:"/post-jobs",
+            element:<PostJobs/>
+          }
+        ]
+      }
+    ]
+  )
   return (
-    <Button>Login</Button>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <RouterProvider router={router}/>
+    </ThemeProvider>
   )
 }
 
