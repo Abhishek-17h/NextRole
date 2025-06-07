4f9e05c1057f780d8a54171aeb1fe6f9b5ce80aa
Abhishek-17H <abhishekhegaje1702@gmail.com>
2025-06-07 12:34:19 +0530

    developed saved job page

--- src/api/apiJobs.js
+++ src/api/apiJobs.js
@@ -107,3 +107,16 @@ export async function addNewJob(token, _, jobData) {
 
   return data;
 }
+export async function getSavedJobs(token) {
+  const supabase = await superbaseClient(token);
+  const { data, error } = await supabase
+    .from("saved_job")
+    .select("*, job: jobs(*, company: companies(name,logo_url))");
+
+  if (error) {
+    console.error("Error fetching Saved Jobs:", error);
+    return null;
+  }
+
+  return data;
+}
